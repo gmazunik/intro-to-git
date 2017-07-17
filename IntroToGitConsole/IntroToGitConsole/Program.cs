a2cf2bdae270c897652f4d25bbd86ccc5e182cd4
Greg Mazunik <gmazunik@gmail.com>
2017-07-17 12:50:30 -0500

    Add lines method

--- IntroToGitConsole/IntroToGitConsole/Program.cs
+++ IntroToGitConsole/IntroToGitConsole/Program.cs
@@ -12,6 +12,15 @@ namespace IntroToGitConsole
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Another damn line...");
+            foreach(var str in DisplayStrings())
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        static string[] DisplayStrings()
+        {
+            return ["String 1", "String 2"];
         }
     }
 }
